91df0a8591a432669aacbda7b8fb55309672005a
Swathi N Rao <37450502+swathiraon@users.noreply.github.com>
2024-02-23 21:39:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from torchvision import datasets, transforms
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+
 def GetCorrectPredCount(pPrediction, pLabels):
   return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
 
@@ -31,7 +32,7 @@ def train(model, device, train_loader, optimizer, criterion,train_losses,train_a
     # Backpropagation
     loss.backward()
     optimizer.step()
-
+    
     correct += GetCorrectPredCount(pred, target)
     processed += len(data)
 
@@ -64,6 +65,7 @@ def test(model, device, test_loader, criterion,test_losses,test_acc):
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
     return [test_acc,test_losses]
 
 def plot(train_losses,train_acc,test_losses,test_acc):
